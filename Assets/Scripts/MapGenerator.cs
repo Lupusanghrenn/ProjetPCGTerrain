d67e727649ab8519520f81ae3e4618543bc3404a
Raphael GAMAGORA <raphael.courbier@univ-lyon2.fr>
2019-12-02 17:16:52 +0100

    ajout octave et seeding

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,6 +8,14 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float mapScale;
 
+    public int octaves;
+    [Range(0f,1f)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     // Start is called before the first frame update
@@ -24,9 +32,22 @@ public class MapGenerator : MonoBehaviour
 
     public void generateMap()
     {
-        float[,] map =Noise.generateNoiseMap(mapWidth, mapHeight, mapScale);
+        float[,] map =Noise.generateNoiseMap(mapWidth, mapHeight,seed, mapScale,octaves,persistance,lacunarity,offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.drawNoiseMap(map);
+    }
+
+    public void OnValidate()
+    {
+        if (lacunarity < 1f)
+        {
+            lacunarity = 1f;
+        }
+
+        if (octaves < 2)
+        {
+            octaves = 2;
+        }
     }
 }
