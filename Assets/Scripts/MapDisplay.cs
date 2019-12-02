302bc99111a9761f8aa684f32f092a947eefe3b5
Raphael GAMAGORA <raphael.courbier@univ-lyon2.fr>
2019-12-02 15:41:05 +0100

    Noice map

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+
+    public Renderer textureRenderer;
+
+    public void drawNoiseMap(float[,] map)
+    {
+        int width = map.GetLength(0);
+        int height = map.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        for(int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                //texture.SetPixel(x, y, new Color(map[x, y], map[x, y], map[x, y]));
+                texture.SetPixel(x, y, Color.Lerp(Color.black,Color.white,map[x,y]));
+            }
+        }
+
+        texture.Apply();
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(width, 1, height);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth;
+    public int mapHeight;
+    public float mapScale;
+
+    public bool autoUpdate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void generateMap()
+    {
+        float[,] map =Noise.generateNoiseMap(mapWidth, mapHeight, mapScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.drawNoiseMap(map);
+    }
+}
