fa93a602fccf18dd38666b3e1ea17d2108248120
Raphael GAMAGORA <raphael.courbier@univ-lyon2.fr>
2019-12-09 09:40:27 +0100

    add generatePositions3D

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -72,7 +72,47 @@ public class MapDisplay : MonoBehaviour
                 }
             }
         }
+    }
 
+    public Vector3[,,] generatePositions3D(float[,,] map)
+    {
+        int width = map.GetLength(0);
+        int height = map.GetLength(1);
+        int deep = map.GetLength(2);
+
+        cubeTransform.localScale = new Vector3(width, height, deep);
+        spheres.transform.position = cubeTransform.position;
+        Vector3 initPos = (cubeTransform.position - cubeTransform.forward * deep / 2) - cubeTransform.right * width / 2 - cubeTransform.up * height / 2;
+        initPos += cubeTransform.right / 2;
+        initPos += cubeTransform.forward / 2;
+        initPos += cubeTransform.up / 2;
+        Vector3 currentPos = initPos;
+        Debug.Log("sphere");
+
+        Vector3[,,] positions = new Vector3[width, height, deep];
+
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                currentPos = initPos;
+                currentPos += cubeTransform.right * x;
+                currentPos += cubeTransform.up * y;
+                for (int z = 0; z < deep; z++)
+                {
+                    positions[x, y, z] = currentPos;
+                    //texture.SetPixel(x, y, new Color(map[x, y], map[x, y], map[x, y]));
+                    /*GameObject go = Instantiate(prefab, currentPos, Quaternion.identity, spheres.transform);
+                    Material mat = new Material(matPrefab);
+                    mat.color = Color.Lerp(Color.black, Color.white, map[x, y, z]);
+                    go.GetComponent<MeshRenderer>().sharedMaterial = mat;*/
+                    currentPos += cubeTransform.forward;
+                }
+            }
+        }
+
+        return positions;
     }
     // Start is called before the first frame update
     void Start()
